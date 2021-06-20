f8f2d3f33431c6cd215648cfe659c7c7f27cc1de
skysan <26142873+skysan87@users.noreply.github.com>
2021-06-20 18:00:16 +0900

    initial commit

---
+++ web/nuxt.config.js
@@ -0,0 +1,61 @@
+const devConfig = {
+  ssr: false,
+
+  target: 'static',
+
+  head: {
+    title: 'timer',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+  ],
+
+  plugins: [
+  ],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  modules: [
+  ],
+
+  build: {
+  }
+}
+
+const prodConfig = {
+  ...devConfig,
+
+  router: {
+    ...devConfig.router,
+    base: './',
+    mode: 'hash'
+  },
+
+  generate: {
+    ...devConfig.generate,
+    dir: '../app/dist'
+  },
+
+  build: {
+    ...devConfig.build
+  }
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+
+export default config
